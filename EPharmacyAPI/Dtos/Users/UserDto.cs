7fa72fbfe09f44edc7314001393d6258a58e436b
Zzznai <naimabazabaz@gmail.com>
2025-12-31 21:47:08 +0200

    Reorganize users DTO models to separate folder

---
+++ EPharmacyAPI/Dtos/Users/UserDto.cs
@@ -0,0 +1,9 @@
+namespace EPharmacyAPI.Dtos.Users;
+
+public class UserDto
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
